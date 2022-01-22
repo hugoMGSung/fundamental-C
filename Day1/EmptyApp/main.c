990daa15e1cae3bf6900fafb4763663c4fe8a900
HugoMGSung <personar95@naver.com>
2022-01-22 20:55:40 +0900

    내용 테스트

---
+++ Day1/EmptyApp/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void) {
+	printf("Hello, World.\n");
+	printf("¼¼»ó¾Æ, ¾È³ç!\n");
+
+	float pi;
+	pi = 3.141592654;
+
+	printf("%d size\n", sizeof(pi));
+	printf("%.9f value\n", pi);
+}
